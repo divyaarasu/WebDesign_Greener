a610b296d980286026100f3980603f18ce54eeba
Rashmi Rajendra Joshi <joshi.ras@husky.neu.edu>
2019-12-09 20:18:25 -0500

    initial commit

---
+++ models/BillData.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const BillSchema = new Schema({
+  userid: {
+    type: String,
+    required: true
+  },
+  month: {
+    type: String,
+    required: true
+  },
+  watts: {
+    type: Number,
+    required: true
+  }
+});
+module.exports = Bill = mongoose.model("billData", BillSchema);
---
+++ routes/api/billData.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+// Load User model
+const Bill = require("../../models/BillData");
+
+router.post("/submitbill", (req, res) => {
+    // Form validation
+        const newbill = new Bill({
+          userid: req.body.userid,
+          month: req.body.month,
+          watts: req.body.watts
+        });
+        newbill.save().then(console.log(res));
+});
+router.get("/submitbill", (req, res) => {
+    // Form validation
+    Bill.find({}, function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    })
+});
+module.exports = router;
