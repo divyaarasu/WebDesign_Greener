b86b3554ea4f3aeff8943db5f3a8f88db8f8bdd2
Chandrakanth <apple@Apples-MacBook-Pro.local>
2019-12-10 14:55:49 -0500

    Products page

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -3,6 +3,51 @@ import './Products.css';
 import { Jumbotron, Carousel, Card, Button, Container, Row, Col } from 'react-bootstrap';
 
 class Products extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      products:[{
+        Title:"Kitchen Compost Bin",
+        Img:"../../assets/images/dustbin.jpg",
+        Desc:"Instead of the trash can, is a great eco-friendly change to make at home.",
+        Rating:4,
+        SellerName:"a"
+      },{
+        Title:"Food Storage Bag",
+        Img:"../../assets/images/bag.jpg",
+        Desc:"Your plastic food-storage bag use stops here. Available in a variety of sizes.",
+        Rating:4,
+        SellerName:"b"
+      },{
+        Title:"Hydro Flask",
+        Img:"../../assets/images/bottleProduct.jpg",
+        Desc:"A durable, non-disposable water bottle is what you should be using on the daily.",
+        Rating:4,
+        SellerName:"c"
+      },{
+        Title:"Bamboo Cutlery Set",
+        Img:"../../assets/images/shampoo.jpg",
+        Desc:"Disposable flatware is often a double whammy of plastic use wrapped up in even more plastic.",
+        Rating:4,
+        SellerName:"d"
+      },{
+        Title:"All Purpose Spaghetti Scrub",
+        Img:"../../assets/images/detergent.jpg",
+        Desc:"Goodbye Detergent! This product works astonishingly well, and is much less wasteful.",
+        Rating:4,
+        SellerName:"e"
+      },{
+        Title:"Compostable Plates",
+        Img:"../../assets/images/plates.jpg",
+        Desc:"Not only are they just as durable, but they have the added bonus of being dye-free.",
+        Rating:4,
+        SellerName:"f"
+      }]
+    }
+  }
+
+  
+
   render(){
     return(
         <div>
@@ -10,94 +55,20 @@ class Products extends React.Component{
         <video class="video-fluid z-depth-1 video-background" autoplay="autoplay" loop="loop" controls="controls" muted="muted" id="vid">
         <source src="https://mdbootstrap.com/img/video/Sail-Away.mp4" type="video/mp4" />
         </video>
-            
-        <div class="container">
-          <div class="row">
-            <div class="col-sm product">
-                <Card>
-                <Card.Img variant="top" src="../../assets/images/bottleProduct.jpg"/>
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </div>
-            <div class="col-sm product">
-                <Card>
-                <Card.Img variant="top" src="../../assets/images/bottleProduct.jpg" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </div>
-            <div class="col-sm product">
-                <Card>
-                <Card.Img variant="top" src="../../assets/images/bottleProduct.jpg" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card> 
-            </div>
-          </div>
 
-          <div class="row">
-            <div class="col-sm rP">
-                <Card>
-                <Card.Img variant="top" src="../../assets/images/bottleProduct.jpg" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </div>
-            <div class="col-sm rP">
-                <Card>
-                <Card.Img variant="top" src="../../assets/images/bottleProduct.jpg" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </div>
-            <div class="col-sm rP">
-                <Card>
-                <Card.Img variant="top" src="../../assets/images/bottleProduct.jpg" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card> 
-            </div>
-          </div>
-        </div>
 
-        </div>
+    <div class="container">
+        {this.state.products.map((p,i) => (
+          ((i+1)%4==0) ? (<div className="row">
+          <div className="col-sm-3 product">
+              Hey!
+              </div>
+            </div>) : (<div className="col-sm-3 product">
+              Hey!
+            </div>)
+        ))}
+      </div>
+      </div>
     );
   }
 }
