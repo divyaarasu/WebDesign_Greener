cd2205fb75ad87bb645265e156753d1e6669da70
Vinay Srivatsan Srinivasan <srinivasan.vi@husky.neu.edu>
2019-12-10 19:10:46 -0500

    adding info

--- client/src/components/Info/Info.js
+++ client/src/components/Info/Info.js
@@ -8,6 +8,7 @@ function Info() {
     return (
       
       <div id="b">
+      
       <div id="awarness"><h1><i>Awareness</i></h1></div>
       <div class="row">
   <div class="col-sm-5 col-md-6">
@@ -21,21 +22,33 @@ function Info() {
   
   </div>
   <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
+  
   <h1><i><center>Wind Energy</center></i></h1>
-  <p><q>Solar energy is the transformation of heat energy that comes from the sun to electricity. It has been used for thousands of years in many different ways by people all over the world. As well as its traditional human uses in heating, cooking, and drying, it is used today to make electricity where other power supplies are absent, such as in remote places and in space.</q></p>
-  <button type="button" class="btn btn-primary">Read More</button>
+  <div id="expl">
+  <p><q>Wind power or wind energy is the use of wind to provide the mechanical power through wind turbines to turn electric generators and traditionally to do other work, like milling or pumping. Wind power is a sustainable and renewable energy, and has a much smaller impact on the environment compared to burning fossil fuels.</q></p>  
+  </div>
+  
+  <div id='button'>
   <Link to="/windInfo">Learn More</Link>
+  
+  
+  </div>
+  
   </div>
 </div>
 <div><h1>                                                </h1></div>
 <div class="row">
 <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
 <div></div>
-<h1><i>Wind Energy</i></h1>
-<p><q>Solar energy is the transformation of heat energy that comes from the sun to electricity. It has been used for thousands of years in many different ways by people all over the world. As well as its traditional human uses in heating, cooking, and drying, it is used today to make electricity where other power supplies are absent, such as in remote places and in space.</q></p>
+<h1><i>Solar Energy</i></h1>
+<p><q>It is an important source of renewable energy and its technologies are broadly characterized as either passive solar or active solar depending on how they capture and distribute solar energy or convert it into solar power.</q></p>
 
 
-<button type="button" class="btn btn-primary" >Read More</button>
+<div id='button'>
+  <Link to="/windInfo">Learn More</Link>
+  
+  
+  </div>
 
 
 <div></div>
@@ -53,8 +66,59 @@ function Info() {
 
 </div>
 <div class="row">
-  <div class="col-sm-6 col-md-5 col-lg-6">.col-sm-6 .col-md-5 .col-lg-6</div>
-  <div class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">.col-sm-6 .col-md-5 .offset-md-2 .col-lg-6 .offset-lg-0</div>
+<div class="col-sm-5 col-md-6">
+<img
+            className="d-block w-100"
+            id="a"
+            src={carousel1}
+            alt="First slide"
+          />
+
+
+</div>
+<div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
+
+<h1><i><center>Wind Energy</center></i></h1>
+<div id="expl">
+<p><q>Wind power or wind energy is the use of wind to provide the mechanical power through wind turbines to turn electric generators and traditionally to do other work, like milling or pumping. Wind power is a sustainable and renewable energy, and has a much smaller impact on the environment compared to burning fossil fuels.</q></p>  
+</div>
+
+<div id='button'>
+<Link to="/windInfo">Learn More</Link>
+
+
+</div>
+
+</div>
+</div>
+<div><h1>                                                </h1></div>
+<div class="row">
+<div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
+<div></div>
+<h1><i>Solar Energy</i></h1>
+<p><q>It is an important source of renewable energy and its technologies are broadly characterized as either passive solar or active solar depending on how they capture and distribute solar energy or convert it into solar power.</q></p>
+
+
+<div id='button'>
+<Link to="/windInfo">Learn More</Link>
+
+
+</div>
+
+
+<div></div>
+</div>
+<div class="col-sm-5 col-md-6">
+<img
+          className="d-block w-100"
+          id="a"
+          src={carousel1}
+          alt="First slide"
+        />
+
+
+</div>
+
 </div>
 </div>
 
